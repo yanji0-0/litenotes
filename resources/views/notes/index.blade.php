f7a820120701029ef1e5d7084cd94acdf370903c
yanji0-0 <yannjstine0.0@gmail.com>
2026-03-24 22:04:50 +0800

    first commit

---
+++ resources/views/notes/index.blade.php
@@ -0,0 +1,31 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Notes') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                @forelse ($notes as $note)    
+                <div class="mb-6 p-6 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        <h2 class="font-bold text-2xl text-gray-900 dark:text-white">
+                            {{ $note->title }}
+                        </h2>
+                        <p class="mt-2 text-gray-700 dark:text-gray-300">
+                            {{ Str::limit($note->text, 200) }}
+                        </p>
+
+                        <span class="block mt-4 text-sm text-gray-600 dark:text-gray-400">{{ $note->updated_at->diffForHumans() }}</span>
+                </div>
+                
+            @empty
+                <p class="text-gray-700 dark:text-gray-300">You have no notes yet.</p>
+
+                @endforelse
+
+                {{ $notes->links() }}
+         </div>
+        </div>
+    </div>
+</x-app-layout>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+        <!-- Styles / Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+      
+    </head>
+    <body class="bg-[#FDFDFC]  text-[#1b1b18] min-h-screen p-6 lg:p-8">
+        <header class="w-full text-sm mb-6">
+            @if (Route::has('login'))
+                <nav class="flex items-center justify-end gap-4">
+                    @auth
+                        <a
+                            href="{{ route('notes.index') }}"
+                            class="inline-block px-5 py-1.5  border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
+                        >
+                            Notes
+                        
+                        </a>
+                    @else
+                        <a
+                            href="{{ route('login') }}"
+                            class="inline-block px-5 py-1.5  text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal"
+                        >
+                            Log in
+                        </a>
+
+                        @if (Route::has('register'))
+                            <a
+                                href="{{ route('register') }}"
+                                class="inline-block px-5 py-1.5  border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
+                                Register
+                            </a>
+                        @endif
+                    @endauth
+                </nav>
+            @endif
+            <h1 class="text-5xl">
+                Litenotes
+            </h1>
+        </header>
+        
+
+        @if (Route::has('login'))
+            <div class="h-14.5 hidden lg:block"></div>
+        @endif
+    </body>
+</html>
